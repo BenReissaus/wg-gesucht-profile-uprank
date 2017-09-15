1f9884901e101a8ea65f5ae17615893ff09d7c7f
Benjamin Reissaus <benjamin.reissaus@gmail.com>
2017-09-15 12:37:51 +0200

    add update_title() function

--- uprank.py
+++ uprank.py
@@ -14,11 +14,11 @@ config = yaml.safe_load(open("config.yml"))
 url = 'https://www.wg-gesucht.de/'
 edit_url = 'https://www.wg-gesucht.de/gesuch-bearbeiten.html?edit={}'.format(config['application_id'])
 
+# allows running it on remote server
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-#browser = webdriver.Chrome(config['path_to_driver'])
-browser = webdriver.Firefox(executable_path=r'/opt/geckodriver')
+browser = webdriver.Chrome(config['path_to_driver'])
 delay = 10
 
 def load_web_page():
@@ -58,6 +58,26 @@ def login():
 
     time.sleep(3)
 
+def update_title():
+    title1 = u'Studium vorbei - suche WG in der Heimat !'
+    title2 = u'Studium vorbei - suche WG in der Heimat!'
+    try:
+        title_element = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, "title")))
+        print "Title input is ready!"
+        current_title = title_element.get_attribute('value')
+        title_element.clear()
+        new_title = title1
+        if current_title == title1:
+            new_title = title2
+
+        title_element.send_keys(new_title)
+        print("Set title to {}".format(new_title))
+        
+    except TimeoutException:
+        print "Loading title input took more than 10 seconds!"
+        sys.exit(1)
+    
+
 
 def update_application():
     # Go to edit page of application
@@ -73,25 +93,8 @@ def update_application():
         print "Loading ad submit button took more than 10 seconds!"
         sys.exit(1)
 
-
     # Update title on second page of application
-    title1 = u'Studium vorbei - suche WG in der Heimat !'
-    title2 = u'Studium vorbei - suche WG in der Heimat!'
-
-    try:
-        title_element = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, "title")))
-        print "Title input is ready!"
-        title = title_element.get_attribute('value')
-        title_element.clear()
-
-        if title == title1:
-            title_element.send_keys(title2)
-        else:
-            title_element.send_keys(title1)
-
-    except TimeoutException:
-        print "Loading title input took more than 10 seconds!"
-        sys.exit(1)
+    update_title()
 
     # Submit change
     browser.find_element_by_id('thisForm').submit()

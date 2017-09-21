22201c372bf6daf932f9eb33439bdc1da983e0f6
Benjamin Reissaus <benjamin.reissaus@gmail.com>
2017-09-21 17:36:28 +0200

    extract waiting for loaded element into method

--- upranking.py
+++ upranking.py
@@ -21,11 +21,9 @@ class Upranking:
         self.logger = logging.getLogger('wg-gesucht')
         self.display = Display(visible=0, size=(800, 600))
         self.edit_url = 'https://www.wg-gesucht.de/gesuch-bearbeiten.html?edit={}'
-        self.config = None
         self.browser = None
         self.username = None
         self.password = None
-
 
     def execute(self):
         self.initialize()
@@ -35,20 +33,21 @@ class Upranking:
         self.shut_down()
 
     def initialize(self):
-        self.load_config()
-
         self.display.start()
-        self.edit_url = self.edit_url.format(self.config['application_id'])
-        self.browser = webdriver.Chrome(self.config['path_to_driver'])
-        self.username = self.config['username']
-        self.password = self.config['password']
-
+        self.load_config_values()
         self.initialize_logger()
 
-    def load_config(self):
+    def load_config_values(self):
         dir = os.path.dirname(__file__)
         config_file_path = os.path.join(dir,"config.yml")
-        self.config = yaml.safe_load(open(config_file_path))
+        config = yaml.safe_load(open(config_file_path))
+
+        self.edit_url = self.edit_url.format(config['application_id'])
+        self.browser = webdriver.Chrome(config['path_to_driver'])
+        self.username = config['username']
+        self.password = config['password']
+
+        self.logger.info("Successfully loaded config values.")
 
     def initialize_logger(self):
         formatter = logging.Formatter('%(asctime)s - %(message)s')
@@ -58,29 +57,28 @@ class Upranking:
         self.logger.setLevel(logging.INFO)
         self.logger.addHandler(fh)
 
+    def wait_till_element_loaded(self, element_name, xpath_type, xpath_value):
+        try:
+            condition = EC.presence_of_element_located((xpath_type, xpath_value))
+            element = WebDriverWait(self.browser, self.delay).until(condition)
+            self.logger.info("{} has loaded.".format(element_name))
+        except TimeoutException:
+            self.logger.warn("Loading {} took more than {} seconds!".format(element_name, self.delay))
+            self.shut_down()
+            sys.exit(1)
+        else:
+            return element
+
     def load_web_page(self):
         self.browser.get(self.url)
-        try:
-            WebDriverWait(self.browser, self.delay).until(EC.presence_of_element_located((By.XPATH, "//div[@id='service-navigation']/div/a[2]")))
-            self.logger.info("Page has loaded!")
-        except TimeoutException:
-            self.logger.warn("Loading took more than {} seconds!".format(self.delay))
-            self.shut_down()
-            sys.exit(1)
+        self.wait_till_element_loaded("Web Page", By.XPATH, "//div[@id='service-navigation']/div/a[2]")
 
     def login(self):
-        # Click on Login Link
+
+        # click on login link
         self.browser.find_element(By.XPATH, "//div[@id='service-navigation']/div/a[2]").click()
 
-        try:
-            WebDriverWait(self.browser, self.delay).until(EC.presence_of_element_located((By.ID, "login_email_username")))
-            self.logger.info("Login modal has loaded!")
-        except TimeoutException:
-            self.logger.warn("Loading the modal took more than {} seconds!".format(self.delay))
-            self.shut_down()
-            sys.exit(1)
-
-        # Make sure the login input fields are loaded and visible by simulating a click
+        # make sure the login input fields are loaded and visible by simulating a click
         assert 'login_email_username' in self.browser.page_source
         action = action_chains.ActionChains(self.browser)
         action.send_keys(keys.Keys.COMMAND+keys.Keys.ALT+'i')
@@ -90,59 +88,46 @@ class Upranking:
         action.send_keys("document.querySelector('#login_email_username').click()"+keys.Keys.ENTER)
         action.perform()
 
-        # Login
+        # login
         self.browser.find_element_by_id('login_email_username').send_keys(self.username)
         self.browser.find_element_by_id('login_password').send_keys(self.password)
         self.browser.find_element_by_id('login_basic').submit()
 
         time.sleep(3)
 
+        self.logger.info("Logged in.")
+
     def update_title(self):
         title1 = u'Studium vorbei - suche WG in der Heimat !'
         title2 = u'Studium vorbei - suche WG in der Heimat!'
-        try:
-            title_element = WebDriverWait(self.browser, self.delay).until(EC.presence_of_element_located((By.ID, "title")))
-            self.logger.info("Title input is ready!")
-            current_title = title_element.get_attribute('value')
-            title_element.clear()
-            new_title = title1
-            if current_title == title1:
-                new_title = title2
+        self.wait_till_element_loaded("Title input", By.ID, "title")
+        title_element = self.browser.find_element_by_id('title')
 
-            title_element.send_keys(new_title)
-            self.logger.info("Set title to {}".format(new_title))
+        current_title = title_element.get_attribute('value')
+        title_element.clear()
+        new_title = title1
+        if current_title == title1:
+            new_title = title2
 
-        except TimeoutException:
-            self.logger.warn("Loading title input took more than {} seconds!".format(self.delay))
-            self.shut_down()
-            sys.exit(1)
+        title_element.send_keys(new_title)
+        self.logger.info("Set title to '{}'".format(new_title))
+
 
     def update_application(self):
-        # Go to edit page of application
         self.browser.get(self.edit_url)
+        self.wait_till_element_loaded("First edit page", By.ID, "create_ad")
 
-        # Leave and submit first page of application as is
-        try:
-            first_page = WebDriverWait(self.browser, self.delay).until(EC.presence_of_element_located((By.ID, "create_ad")))
-            self.logger.info("Ad submit button is ready!")
-            first_page.submit()
+        # leave first edit page untouched
+        self.browser.find_element_by_id('create_ad').submit()
 
-        except TimeoutException:
-            self.logger.warn("Loading ad submit button took more than {} seconds!".format(self.delay))
-            self.shut_down()
-            sys.exit(1)
-
-        # Update title on second page of application
         self.update_title()
-
-        # Submit change
         self.browser.find_element_by_id('thisForm').submit()
+
+        self.logger.info("Finished update!")
 
     def shut_down(self):
         self.browser.close()
         self.display.stop()
-        self.logger.info("Finished update!")
-
 
 if __name__ == '__main__':
     upranking = Upranking()
